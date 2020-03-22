922dc82998dae1b9c9bc8ad00dad3ac0de16579f
Jonathan Hayden <jonathanhayden@jons-mbp.home>
2020-03-22 21:51:38 +0000

    fix terraform structure

--- remote_state_tf/defaults.tf
+++
@@ -1,9 +0,0 @@
-variable "remote_state_bucket" {
-  type    = string
-  default = "terraform-remote-state-cdah"
-}
-
-variable "remote_state_table" {
-  type    = string
-  default = "terraform-state-locks"
-}
