0c7a40cd962603aee8082dd2324c65b5be8ee5e7
Jonathan Hayden <jonathanhayden@jons-mbp.home>
2020-03-22 21:54:19 +0000

    add missing defaults file

--- terraform/defaults.tf
+++ terraform/defaults.tf
@@ -42,3 +42,13 @@ variable "enable_vpn_gateway" {
   type    = string
   default = false
 }
+
+variable "remote_state_bucket" {
+  type    = string
+  default = "terraform-remote-state-cdah"
+}
+
+variable "remote_state_table" {
+  type    = string
+  default = "terraform-state-locks"
+}
